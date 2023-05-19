1a82813213044d42346c902ff4413fd57de8ba49
Keiran <keiranosullivan1712@gmail.com>
2023-05-19 18:59:07 +0100

    meta

--- cogs/meta.py
+++ cogs/meta.py
@@ -16,7 +16,8 @@ class Meta(commands.Cog):
     async def querydb(self, ctx, *, query: str):
         async with aiosqlite.connect("data.db") as db:
             async with db.execute(query) as Data:
-                for detail in Data:
+                entry = await Data.fetchall()
+                for detail in entry:
                     await ctx.send(content=detail)
 
 def setup(bot):
